299e0c21ae231a05493ef1536383ecbc7dbb336d
Charles Hagen <charlesjhagen@gmail.com>
2024-09-25 00:57:34 -0600

    Fix incorrect minimum version

--- InjectAutofocus/Properties/AssemblyInfo.cs
+++ InjectAutofocus/Properties/AssemblyInfo.cs
@@ -24,7 +24,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © 2024 Charles Hagen")]
 
 // The minimum Version of N.I.N.A. that this plugin is compatible with
-[assembly: AssemblyMetadata("MinimumApplicationVersion", "3.1.9.9001")]
+[assembly: AssemblyMetadata("MinimumApplicationVersion", "3.1.2.9001")]
 
 // The license your plugin code is using
 [assembly: AssemblyMetadata("License", "MPL-2.0")]
